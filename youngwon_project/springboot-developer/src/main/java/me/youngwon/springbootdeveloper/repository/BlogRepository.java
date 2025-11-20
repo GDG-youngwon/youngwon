61e223e43c85e7268cfa11770e0bf1d4cf2df03c
EricaLeeJoowon <leejoowon03@gmail.com>
2025-11-20 15:00:14 +0900

    chore :패키지명 youngwon으로 변경

---
+++ youngwon_project/springboot-developer/src/main/java/me/youngwon/springbootdeveloper/repository/BlogRepository.java
@@ -0,0 +1,7 @@
+package me.youngwon.springbootdeveloper.repository;
+
+import me.youngwon.springbootdeveloper.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+}
